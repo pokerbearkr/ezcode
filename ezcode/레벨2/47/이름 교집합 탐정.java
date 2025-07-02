98cfc62efeb2d848ba65212ffa8e61019a04540a
dongwon oh <ogdongwon@gmail.com>
2025-07-02 17:33:22 +0900

    문제 47. 이름 교집합 탐정 (레벨2) – 메모리: 9853KB, 시간: 73ms

---
+++ ezcode/레벨2/47/이름 교집합 탐정.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken()); // 듣도 못한 사람 수
+        int M = Integer.parseInt(st.nextToken()); // 보도 못한 사람 수
+
+        Set<String> unheard = new HashSet<>();
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            unheard.add(br.readLine());
+        }
+
+        for (int i = 0; i < M; i++) {
+            String name = br.readLine();
+            if (unheard.contains(name)) {
+                result.add(name);
+            }
+        }
+
+        Collections.sort(result);
+
+        System.out.println(result.size());
+        for (String name : result) {
+            System.out.println(name);
+        }
+    }
+}
