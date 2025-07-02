7cff7fb8ab9a4c41903770430e8bc676139b78d4
dongwon oh <ogdongwon@gmail.com>
2025-07-02 16:35:28 +0900

    문제 29. compare (레벨1) – 메모리: 10549KB, 시간: 93ms

---
+++ ezcode/레벨1/29/compare.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().trim().split("\\s+");
+
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+
+        if (A > B) {
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        } else {
+            System.out.println("=");
+        }
+    }
+}
